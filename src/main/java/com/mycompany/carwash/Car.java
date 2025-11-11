faa04c33a27603d2023138253e7f87eff76801c3
Ahmed <ahmed.mustafa.talaat@gmail.com>
2025-11-11 08:17:24 +0200

    Final

--- src/main/java/com/mycompany/carwash/Car.java
+++ src/main/java/com/mycompany/carwash/Car.java
@@ -11,3 +11,30 @@ import java.util.Queue;
  * @author lightum
  */
 // Car.java
+class Car extends Thread {
+    private final int number;
+    private Queue<Car> queue;
+    private Semaphore mutex, empty, full;
+
+    public Car(int id, Queue<Car> queue, Semaphore mutex, Semaphore empty, Semaphore full) {
+        this.number = id;
+        this.queue = queue;
+        this.mutex = mutex;
+        this.empty = empty;
+        this.full = full;
+    }
+    
+    public int getNumber(){
+        return this.number;
+    }
+
+    public void run() {
+        System.out.println("Car " + number + " arrives.");
+        empty.P();
+        mutex.P();
+        queue.add(this);
+        System.out.println("Car " + number + " enters waiting queue.");
+        mutex.V();
+        full.V();
+    }
+}
--- src/main/java/com/mycompany/carwash/Pump.java
+++ src/main/java/com/mycompany/carwash/Pump.java
@@ -12,3 +12,36 @@ import java.util.Queue;
  */
 // Pump.java
 
+class Pump extends Thread {
+    private int id;
+    private Queue<Car> queue;
+    private Semaphore mutex, empty, full;
+
+    public Pump(int id, Queue<Car> queue, Semaphore mutex, Semaphore empty, Semaphore full) {
+        this.id = id;
+        this.queue = queue;
+        this.mutex = mutex;
+        this.empty = empty;
+        this.full = full;
+    }
+
+    public void run() {
+        while (true) {
+            try {
+                full.P();
+                mutex.P();
+                Car car = queue.poll();
+                System.out.println("Pump" + id + ": " + "Car " + car.getNumber() + " login");
+                System.out.println("Pump " + id + " starts servicing Car " + car.getNumber());
+                mutex.V();
+                empty.V();
+
+                Thread.sleep((int) (Math.random() * 4000 + 2000));
+                System.out.println("Pump " + id + " finished servicing Car " + car.getNumber());
+                System.out.println("Bay " + id + " is now free");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
